d3b56feb91476c70f6461816617dff1ae0019abc
Teja36 <bommarajutej@gmail.com>
2024-03-07 14:45:23 -0500

    add cloudinary to next config

--- next.config.js
+++ next.config.js
@@ -8,6 +8,12 @@ const nextConfig = {
         port: "",
         pathname: "/a/**",
       },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        port: "",
+        pathname: "/garden-up/**",
+      },
     ],
   },
 };
