03d82bde82da728454f3dd57b089762f72ec9b10
Teja36 <bommarajutej@gmail.com>
2024-03-10 17:31:48 -0400

    fix a minor bug

--- src/components/CartItemCountIndicator.tsx
+++ src/components/CartItemCountIndicator.tsx
@@ -1,12 +1,12 @@
 "use client";
 
 import { useCartStore } from "@/store/cart";
-import useStore from "../hooks/utils/useStore";
+import useStore from "../hooks/useStore";
 
 const CartItemCountIndicator = () => {
   const count = useStore(useCartStore, (state) => state.cart.length);
 
-  if (count === 0) return null;
+  if (count === undefined || count === 0) return null;
 
   return (
     <span className="absolute -top-2 -right-2 w-4 h-4 rounded-full text-xs text-center bg-white text-green-700">
